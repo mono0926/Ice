ad3515fad8c2bed724eadaa6613f4c6dee648931
Masayuki Ono <mono0926@gmail.com>
2014-05-06 16:51:04 +0900

    トップページ更新

--- web.js
+++ web.js
@@ -1,14 +1,21 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+const
+    express = require("express"),
+    logfmt = require("logfmt"),
+    fs = require("fs"),
+    path = require('path'),
+    app = express();
 
 app.use(logfmt.requestLogger());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  res.send('ice.me');
+    fs.readFile('./index.html', 'UTF-8', function(err, data) {
+
+        res.send(data);
+    })
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
