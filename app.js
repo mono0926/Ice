16efdfb220324d4df4a0926cde341f6932173e7b
Masayuki Ono <mono0926@gmail.com>
2014-05-10 13:06:17 +0900

    redis導入

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const
     routes = require('./routes/index'),
     users = require('./routes/users'),
     ices = require('./routes/ices'),
+    session = require('express-session'),
+    redisClient = require('redis').createClient()
+    RedisStore = require('connect-redis')(session),
     app = express();
 
 // view engine setup
@@ -22,12 +25,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// TODO: hogeじゃなくする
+app.use(cookieParser("hoge"));
+app.use(session({
+    secret: "hoge",
+    store: new RedisStore({client: redisClient})
+}));
+
 app.use(require('stylus').middleware({ src: path.join(__dirname, 'public') }));
 
 app.use('/', routes);
 app.use('/users', users);
 app.use('/ices', ices);
 
+// TODO: デバッグ時のみにする
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -35,28 +46,6 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+app.redisClient = redisClient;
 
 module.exports = app;
